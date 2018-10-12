331761a4283dca1d2b178cf6781310aa9d9cb053
liujun <422134235@qq.com>
2018-10-12 09:52:05 +0800

    添加移除尾节点的代码

--- src/main/java/com/liujun/datastruct/linkedlist/MyLinkedList.java
+++ src/main/java/com/liujun/datastruct/linkedlist/MyLinkedList.java
@@ -21,21 +21,21 @@ public class MyLinkedList {
 
     private Node next;
 
-    public Node(Integer value, Node next) {
+    public Node(Integer value) {
       this.value = value;
-      this.next = next;
     }
 
-    public Integer getValue() {
-      return value;
+    @Override
+    public String toString() {
+      final StringBuilder sb = new StringBuilder("Node{");
+      sb.append("value=").append(value);
+      sb.append('}');
+      return sb.toString();
     }
   }
 
   /** 头链表信息 */
-  private Node headNode = new Node(null, null);
-
-  /** 当前节点信息 */
-  private Node currNode = headNode;
+  private Node headNode = new Node(null);
 
   /**
    * 添加节点信息
@@ -43,9 +43,106 @@ public class MyLinkedList {
    * @param value
    */
   public void add(int value) {
-    Node node = new Node(value, null);
-    currNode.next = node;
-    currNode = node;
+    Node node = new Node(value);
+    headNode.next = node;
+  }
+
+  /**
+   * 根据值查找节点
+   *
+   * @param value 传递的值信息
+   * @return 节点信息
+   */
+  public Node findByValue(int value) {
+    Node temNode = headNode.next;
+
+    while (temNode != null) {
+      if (temNode.value == value) {
+        return temNode;
+      } else {
+        temNode = temNode.next;
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * 根据索引查找节点
+   *
+   * @param index 索引信息
+   * @return 节点信息
+   */
+  public Node findByIndex(int index) {
+    if (index < 0) {
+      return null;
+    }
+
+    Node temp = headNode.next;
+
+    int tmpIndex = 0;
+
+    while (tmpIndex != index && temp != null) {
+      temp = temp.next;
+      ++tmpIndex;
+    }
+
+    return temp;
+  }
+
+  public void insertToHead(int value) {
+
+    Node insNode = new Node(value);
+
+    this.insertToHead(insNode);
+  }
+
+  /**
+   * 插入节点操作
+   *
+   * @param node 需要插入的节点信息
+   */
+  private void insertToHead(Node node) {
+    if (node != null) {
+      Node tmp = headNode.next;
+      headNode.next = node;
+      node.next = tmp;
+    }
+  }
+
+  /**
+   * 在指定节点的后面插入数节点
+   *
+   * @param after 指定的节点信息
+   * @param value 指定的节点值
+   */
+  public void insertToAfter(Node after, int value) {
+    Node newValue = new Node(value);
+    this.insertToAfter(after, newValue);
+  }
+
+  /**
+   * 在指定节点插入后
+   *
+   * @param after
+   * @param newNode
+   */
+  private void insertToAfter(Node after, Node newNode) {
+    newNode.next = after.next;
+    after.next = newNode;
+  }
+
+  /**
+   * 在指定节点的前面插入节点
+   *
+   * @param before 之前的节点信息
+   * @param newNode 新的节点信息
+   */
+  private void insertToBefore(Node before, Node newNode) {
+    Node tmpHead = headNode;
+
+    // 如果当前为头节点，则需要特殊处理，不能直接插入到头
+
   }
 
   /**
@@ -54,25 +151,34 @@ public class MyLinkedList {
    * @return
    */
   public Integer removeLast() {
-    // 1,移除除节点，首先需要找到尾节点的上一个节点
-    Node temNode = headNode;
-    while (temNode.next != null) {
-      if (temNode.next == currNode) {
-        break;
-      } else {
-        temNode = temNode.next;
-      }
+
+    // 1,移除尾节点，需要从头节点开始遍历找到尾节点
+    Node lastNode = headNode;
+
+    while (lastNode != null && lastNode.next != null) {
+      lastNode = lastNode.next;
     }
 
-    // 然后检查是否为头节点，头节点固定，不能删除
-    if (currNode != headNode) {
+    // 再次找到尾节点的上一个节点
+    Node lastNodeTop = headNode;
 
-      Integer value = temNode.next.value;
-      temNode.next = null;
-      currNode = temNode;
-      return value;
-    } else {
+    while (lastNodeTop != null && lastNodeTop.next != lastNode) {
+      lastNodeTop = lastNodeTop.next;
+    }
+
+    //如果上一个节点为头节点，不能删除
+    if(lastNode == headNode)
+    {
       return null;
     }
+
+    //然后再将节点进行修改，将尾节点的上一个节点指向空
+    Integer resultValue = lastNode.value;
+    lastNodeTop.next = null;
+
+
+    return resultValue;
+
+
   }
 }
--- src/test/java/com/liujun/datastruct/linkedlist/MyLinkedListTest.java
+++ src/test/java/com/liujun/datastruct/linkedlist/MyLinkedListTest.java
@@ -1,5 +1,6 @@
 package com.liujun.datastruct.linkedlist;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -9,8 +10,19 @@ import org.junit.Test;
  */
 public class MyLinkedListTest {
 
+  /** 测试添加节点 */
   @Test
   public void testMyLinkedListAdd() {
+    MyLinkedList list = new MyLinkedList();
+
+    list.add(10);
+    list.add(20);
+  }
+
+  /** 测试删除尾节点 */
+  @Test
+  public void testMylinkedListRemoveLast() {
+
     MyLinkedList list = new MyLinkedList();
 
     list.add(10);
@@ -18,15 +30,80 @@ public class MyLinkedListTest {
 
     Integer value = list.removeLast();
     System.out.println(value);
+    System.out.println("2");
 
     value = list.removeLast();
     System.out.println(value);
 
     value = list.removeLast();
     System.out.println(value);
+  }
 
+  /** 测试查找节点所对应的node */
+  @Test
+  public void testMyLinkedListFindByValue() {
+    MyLinkedList list = new MyLinkedList();
+    list.add(10);
+    list.add(20);
+    list.add(30);
+    list.add(40);
+    list.add(50);
 
-    value = list.removeLast();
-    System.out.println(value);
+    MyLinkedList.Node node = list.findByValue(50);
+
+    System.out.println(node);
+    Assert.assertNotNull(node);
+
+    MyLinkedList.Node node1 = list.findByValue(90);
+
+    System.out.println(node1);
+
+    Assert.assertNull(node1);
+  }
+
+  @Test
+  public void testMyLinkedListFindByIndex() {
+    MyLinkedList list = new MyLinkedList();
+    list.add(10);
+    list.add(20);
+    list.add(30);
+
+    MyLinkedList.Node index0 = list.findByIndex(0);
+    System.out.println(index0);
+
+    index0 = list.findByIndex(1);
+    System.out.println(index0);
+
+    index0 = list.findByIndex(2);
+    System.out.println(index0);
+
+    index0 = list.findByIndex(3);
+    System.out.println(index0);
+  }
+
+  @Test
+  public void testInsertToHead() {
+    MyLinkedList list = new MyLinkedList();
+    list.add(10);
+    list.add(20);
+    list.add(30);
+
+    list.insertToHead(2);
+
+    System.out.println(list);
+  }
+
+  @Test
+  public void testInsertToAfterValue() {
+    MyLinkedList list = new MyLinkedList();
+    list.add(10);
+    list.add(20);
+    list.add(30);
+
+    MyLinkedList.Node node = list.findByIndex(1);
+
+    list.insertToAfter(node, 22);
+
+    System.out.println(list);
   }
 }
